eb44bdbdfba78c210a51694db46bc7dad65ab525
baiziyu-fe <baiziyu-fe@foxmail.com>
2023-03-12 19:51:36 +0800

    fix(site): add route list

--- site/src/routes/index.ts
+++ site/src/routes/index.ts
@@ -1,2 +1,13 @@
 import * as compRoute from './component.routes';
 import * as docRoute from './doc.routes';
+
+export const routes = [
+  {
+    path: 'component',
+    children: compRoute.routeList
+  },
+  {
+    path: 'doc',
+    children: docRoute.routesList
+  }
+];
